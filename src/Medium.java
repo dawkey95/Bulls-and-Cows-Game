889db7561c20519f2f236a0e43551a91234b14d6
dawkey95 <dawidkeyser95@gmail.com>
2022-04-19 10:38:48 +1200

    Medium Difficulty Update

--- src/Medium.java
+++ src/Medium.java
@@ -1,13 +1,8 @@
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Objects;
-import java.util.Scanner;
 
 public class Medium extends Computer {
+
+    ArrayList<int[]> computerGuesses = new ArrayList<>();
 
     //Override that is the computers guesses when attempting to guess the opponents code.
     @Override
@@ -15,57 +10,14 @@ public class Medium extends Computer {
         this.playerGuess = generateSecretCode();
         this.clue(this.playerGuess, answer.secretCode);
 
-        //This try-catch block contains code that creates, writes to and reads file for guesses made by
-        //the computer on every turn.
-        try {
-            //Creates file AIGuessTracker
-            FileWriter myWriter = new FileWriter("AIGuessTracker.txt", true);
+        int[] guessTracker = generateSecretCode();
 
-            //Creates file with a pathname to FileWriter created file.
-            File f = new File("AIGuessTracker.txt");
-
-            //Writes data to file by appending every guess made by computer.
-            BufferedWriter appendAIGuess = new BufferedWriter(myWriter);
-            appendAIGuess.write(Arrays.toString(this.playerGuess));
-            appendAIGuess.newLine();
-            appendAIGuess.close();
-            System.out.println("Successfully wrote to the file: " + f);
-
-            //Scanner to read data stored in file and check for duplicate guesses
-            Scanner myReader = new Scanner(f);
-            while (myReader.hasNextLine()) {
-
-                //Reads line from file and replaces expressions with "".
-                String data = myReader.nextLine()
-                        .replaceAll(",", "")
-                        .replaceAll("\\[", "")
-                        .replaceAll("\\]", "")
-                        .replaceAll(" ", "");
-
-                char dupeData[] = data.toCharArray();
-                int count;
-
-                for(int i = 0; i < dupeData.length; i++) {
-                    count = 1;
-                    for(int k = i + 1; k < dupeData.length; k++) {
-                        if(dupeData[i] == dupeData[k] && dupeData[i] != ' ') {
-                            count++;
-                            dupeData[k] = '0';
-                        }
-                    }
-
-                    //A character is considered as duplicate if count is greater than 1
-                    if(count > 1 && dupeData[i] != '0')
-                        System.out.println("Testing for dupes: " + dupeData[i]);
-                        generateSecretCode();
-                }
-
-                System.out.println(data);
-            }
-            myReader.close();
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        if(!computerGuesses.contains(guessTracker)) {
+            computerGuesses.add(guessTracker);
+            this.playerGuess = guessTracker;
+        }
+        else {
+            this.playerGuess(playerGuess, answer);
         }
     }
 }
--- src/SaveToFile.java
+++ src/SaveToFile.java
@@ -1,5 +1,65 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public interface SaveToFile {
 
     public String writeToFile();
-    
+
+//    //This try-catch block contains code that creates, writes to and reads file for guesses made by
+//    //the computer on every turn.
+//        try {
+//        //Creates file AIGuessTracker
+//        FileWriter myWriter = new FileWriter("AIGuessTracker.txt", true);
+//
+//        //Creates file with a pathname to FileWriter created file.
+//        File f = new File("AIGuessTracker.txt");
+//
+//        //Writes data to file by appending every guess made by computer.
+//        BufferedWriter appendAIGuess = new BufferedWriter(myWriter);
+//        appendAIGuess.write(Arrays.toString(this.playerGuess));
+//        appendAIGuess.newLine();
+//        appendAIGuess.close();
+//        System.out.println("Successfully wrote to the file: " + f);
+//
+//        //Scanner to read data stored in file and check for duplicate guesses
+//        Scanner myReader = new Scanner(f);
+//        while (myReader.hasNextLine()) {
+//
+//            //Reads line from file and replaces expressions with "".
+//            String data = myReader.nextLine()
+//                    .replaceAll(",", "")
+//                    .replaceAll("\\[", "")
+//                    .replaceAll("\\]", "")
+//                    .replaceAll(" ", "");
+//
+//            char dupeData[] = data.toCharArray();
+//            int count;
+//
+//            for(int i = 0; i < dupeData.length; i++) {
+//                count = 1;
+//                for(int k = i + 1; k < dupeData.length; k++) {
+//                    if(dupeData[i] == dupeData[k] && dupeData[i] != ' ') {
+//                        count++;
+//                        dupeData[k] = '0';
+//                    }
+//                }
+//
+//                //A character is considered as duplicate if count is greater than 1
+//                if(count > 1 && dupeData[i] != '0')
+//                    System.out.println("Testing for dupes: " + dupeData[i]);
+//                generateSecretCode();
+//            }
+//
+//            System.out.println(data);
+//        }
+//        myReader.close();
+//
+//    } catch (IOException e) {
+//        e.printStackTrace();
+//    }
+
 }
